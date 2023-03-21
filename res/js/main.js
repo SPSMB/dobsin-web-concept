fddd9cb7bd1f31e84a8eb86f0c209a83d091bf13
ttoomas <folprecht.to@gmail.com>
2023-03-21 11:01:50 +0100

    css and js cutted

--- res/js/main.js
+++ res/js/main.js
@@ -32,40 +32,7 @@ window.addEventListener('resize', () => {
     document.documentElement.style.setProperty('--nav-height', `${nav.clientHeight}px`);
 })
 
-
-// Weather API
+// Onload
 window.addEventListener('load', async () => {
-    await weatherApi();
-
     document.documentElement.style.setProperty('--nav-height', `${nav.clientHeight}px`);
 })
-
-
-const weatherIcon = document.querySelector('.status__icon');
-const weatherStatus = document.querySelector('.statusWeather');
-const tempStatus = document.querySelector('.statusTemp');
-const humidStatus = document.querySelector('.statusHumid');
-
-async function weatherApi(){
-    try{
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=dobšín&units=metric&lang=cz&appid=7bfeaeea8782e7eb6ed72fb2b8b7cabb`;
-
-        const nonParsed = await fetch(weatherUrl);
-        const data = await nonParsed.json();
-
-        const currWeather = {
-            temp: Math.round(data.main.temp),
-            humidity: data.main.humidity,
-            weatherDesc: data.weather[0].description,
-            weatherIcon: data.weather[0].icon.slice(0, -1)
-        };
-
-        weatherIcon.src = `./res/img/weather/${currWeather.weatherIcon}.png`;
-        weatherStatus.innerText = currWeather.weatherDesc;
-        tempStatus.innerText = currWeather.temp;
-        humidStatus.innerText = currWeather.humidity;
-    }
-    catch(err){
-        console.log(err);
-    }
-}
