4795cd55a8c542772775f9b63d41c713b0f8db4d
ttoomas <folprecht.to@gmail.com>
2023-03-27 20:25:15 +0200

    filter on all devices working

--- res/js/news.js
+++ res/js/news.js
@@ -1,6 +1,8 @@
+// Filter toggle
 const filterOpenBtn = document.querySelector('.filter__openBtn');
 const filterContainer = document.querySelector('.filter__container');
 const filterBx = document.querySelector('.filter__bx');
+const filterIcon = document.querySelector('.filter__icon');
 
 filterOpenBtn.addEventListener('click', () => {
     filterContainer.classList.toggle('filterActive');
@@ -12,3 +14,41 @@ filterOpenBtn.addEventListener('click', () => {
         filterBx.style.animation = "fadeMoveOut 200ms ease-in-out forwards";
     }
 })
+
+
+// Filter function
+const filterBtns = document.querySelectorAll('.filter__btn');
+const newsBxs = document.querySelectorAll('.news__bx');
+const currentFilter = document.querySelector('.filter__selected');
+
+filterBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let currentType = btn.getAttribute('data-filter-type');
+
+        sortNews(currentType);
+
+        filterBx.style.animation = "fadeMoveOut 200ms ease-in-out forwards";
+        filterContainer.classList.toggle('filterActive');
+        currentFilter.innerText = btn.textContent;
+    })
+})
+
+function sortNews(type){
+    if(type === "all"){
+        newsBxs.forEach((newsBx) => {
+            newsBx.style.display = "flex";
+        })
+    }
+    else{
+        newsBxs.forEach((newsBx) => {
+            let newsType = newsBx.getAttribute('data-news-type');
+    
+            if(newsType === type){
+                newsBx.style.display = "flex";
+            }
+            else{
+                newsBx.style.display = "none";
+            }
+        })
+    }
+}
