e5d43c27637ffbfbb2d0bfec5de054f27727c865
ttoomas <folprecht.to@gmail.com>
2023-04-04 21:07:55 +0200

    mandatory info page done, lazy loading on all images

--- res/js/home.js
+++ res/js/home.js
@@ -1,34 +1,36 @@
 // Weather API
-window.addEventListener('load', async () => {
-    await weatherApi();
-})
+const WEATHER_API_KEY = 0;
+
+// window.addEventListener('load', async () => {
+//     await weatherApi();
+// })
 
 
-const weatherIcon = document.querySelector('.status__icon');
-const weatherStatus = document.querySelector('.statusWeather');
-const tempStatus = document.querySelector('.statusTemp');
-const humidStatus = document.querySelector('.statusHumid');
+// const weatherIcon = document.querySelector('.status__icon');
+// const weatherStatus = document.querySelector('.statusWeather');
+// const tempStatus = document.querySelector('.statusTemp');
+// const humidStatus = document.querySelector('.statusHumid');
 
-async function weatherApi(){
-    try{
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=50.489527643619034&lon=15.038305142032598&units=metric&lang=cz&appid=7bfeaeea8782e7eb6ed72fb2b8b7cabb`;
+// async function weatherApi(){
+//     try{
+//         const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=50.489527643619034&lon=15.038305142032598&units=metric&lang=cz&appid=${WEATHER_API_KEY}`;
 
-        const nonParsed = await fetch(weatherUrl);
-        const data = await nonParsed.json();
+//         const nonParsed = await fetch(weatherUrl);
+//         const data = await nonParsed.json();
 
-        const currWeather = {
-            temp: Math.round(data.main.temp),
-            humidity: data.main.humidity,
-            weatherDesc: data.weather[0].description,
-            weatherIcon: data.weather[0].icon.slice(0, -1)
-        };
+//         const currWeather = {
+//             temp: Math.round(data.main.temp),
+//             humidity: data.main.humidity,
+//             weatherDesc: data.weather[0].description,
+//             weatherIcon: data.weather[0].icon.slice(0, -1)
+//         };
 
-        weatherIcon.src = `./res/img/weather/${currWeather.weatherIcon}.png`;
-        weatherStatus.innerText = currWeather.weatherDesc;
-        tempStatus.innerText = currWeather.temp;
-        humidStatus.innerText = currWeather.humidity;
-    }
-    catch(err){
-        console.log(err);
-    }
-}
+//         weatherIcon.src = `./res/img/weather/${currWeather.weatherIcon}.png`;
+//         weatherStatus.innerText = currWeather.weatherDesc;
+//         tempStatus.innerText = currWeather.temp;
+//         humidStatus.innerText = currWeather.humidity;
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
